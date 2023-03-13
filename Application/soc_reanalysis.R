917d9e2cffd698675d2ddf1fc816d3299ac7f54b
Mark J Meyer <mjm556@math-ad-hlejv40.myfiosgateway.com>
2023-03-13 15:28:45 -0400

    Update Application SIBS Accept.

--- Application/soc_reanalysis.R
+++ Application/soc_reanalysis.R
@@ -16,24 +16,13 @@ K     <- ncol(X1)
 B         <- 10000
 burnin    <- B
 
-#### Bayesian models ####
+#### Bayesian MMP ####
 
-# naive model #
-model_bpm     <- bpm(X1, X2, B = B, burnin = burnin, verbose = TRUE)
-summary(model_bpm)
-
-# penalized model #
-model_pbpm    <- pbpm(X1, X2, random = FALSE, B = B, burnin = burnin, 
-                      Al = 10, Ae = 10, hc = TRUE, verbose = TRUE)
-summary(model_pbpm)
-
-# multivariate model #
 model_bmvp    <- bmvp(X1, X2, B = B, burnin = burnin, pen = TRUE,
-                      Al = 1, Ae = 1, hc = TRUE, al = 0.01, bl = 0.01, 
+                      Al = 5, hc = TRUE, al = 0.01, bl = 0.01, 
                       Kp = 2, alpha = 0.001, As = 1, Bs = 1,
                       verbose = TRUE)
 summary(model_bmvp)
-
 
 #### trace plots ####
 labsr    <- c(expression(rho[1]), expression(rho[2]), expression(rho[3]), expression(rho[4]), expression(rho[5]))
@@ -43,4 +32,9 @@ for(i in 1:K){
   abline(h = axTicks(2), v = axTicks(1), lty = 3, col = 'lightgray')
   lines(1:B, model_bmvp$rho[,i], type = 'l', lwd = 2, col = 'forestgreen')
 }
+
+#### GEE and bootstrap-based approach ####
+geek    <- geemmp(X1, X2)
+boot    <- bootmmp(X1, X2, B = 10000)
+
 
